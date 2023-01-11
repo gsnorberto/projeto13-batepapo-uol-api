002cd8bf5cee0d17c0dab3d59541d440f061ef34
“gsnorberto” <sn_gabriel@outlook.com>
2023-01-10 22:42:16 -0300

    feat: Conexão com o MongoDB

--- server.js
+++ server.js
@@ -1,14 +1,37 @@
+import { MongoClient } from 'mongodb'
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import apiRoutes from './src/routes.js'
 
-dotenv.config()
-
 const app = express()
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
+
+dotenv.config()
+
+// MongoDB - Connection
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
+
+async function main() {
+    await mongoClient.connect()
+    console.log("Conectato ao servidor com sucesso!")
+    const db = mongoClient.db("teste")
+    const collection = db.collection('documents')
+
+    // Insert data
+    // const insertResult = await collection.insertOne({name: 'Gabriel s Nor', lastStatus: Date.now()});
+    // console.log('Inserted documents =>', insertResult);
+
+    return "OK!";
+}
+main()
+    .then(console.log)
+    .catch(console.error)
+    .finally(() => mongoClient.close())
+
+
 
 app.use("/", apiRoutes)
 
